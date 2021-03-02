af997c2ae0948b9c42edf386540d5db885882231
Soo-Beom <soobeomkim77@gmail.com>
2021-03-02 11:21:03 -0800

    Completed Assignment

--- main.js
+++ main.js
@@ -3,10 +3,46 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+let modal = document.querySelector("#modal")
+modal.hidden = true
 
 
+let likeButton = document.querySelectorAll(".like-glyph");
+likeButton.forEach(element => element.addEventListener('click', heart))
 
+function heart(e){
+  mimicServerCall()
+  .then(function(resolve){
+    let heartIcon = e.target
+    if (heartIcon.textContent == EMPTY_HEART){
+      setFullHeart(e)
+    } else {
+      setEmptyHeart(e)
+      }
+  })
+  .catch(function(reject){
+    let message = document.querySelector("#modal-message")
+    message.textContent = reject
+    modal.hidden = false
+    setTimeout(hideModal, 5000)
+  })
+}
 
+function hideModal(){
+  modal.hidden = true
+}
+
+function setFullHeart(e){
+  let emptyHeart = e.target
+  emptyHeart.textContent = FULL_HEART
+  emptyHeart.className = "activated-heart"
+}
+
+function setEmptyHeart(e){
+  let fullHeart = e.target
+  fullHeart.textContent = EMPTY_HEART
+  fullHeart.className = ""
+}
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
 //------------------------------------------------------------------------------
